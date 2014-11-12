fa5c5b1c995da2cd7989f9628086e0079c973b64
Philip Jacob <pkjacob@gmail.com>
2014-11-12 16:34:25 +1100

    hangman

---
+++ hangman.js
@@ -0,0 +1,14 @@
+
+
+// Store list of words in an array called "words". 
+var words = ['Andrew', 'Rusli', 'Phil', 'Angela', 'Jacquie']
+
+// Create a function that randomly selects a word from the array.
+
+var word_selector = function() {
+	var random_word = Math.random(words)
+	return random_word
+	debugger
+
+};
+
