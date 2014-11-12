6c0f2dc1a23d19563f1eda139d3268f89f9976ac
Andrew Thompson <andrew@imaginedbyandrew.com>
2014-11-12 16:51:14 +1100

    Pushing in order to pull

--- hangman.js
+++ hangman.js
@@ -1,14 +1,9 @@
-
-
 // Store list of words in an array called "words". 
 var words = ['Andrew', 'Rusli', 'Phil', 'Angela', 'Jacquie']
 
 // Create a function that randomly selects a word from the array.
 
 var word_selector = function() {
-	var random_word = Math.random(words)
-	return random_word
-	debugger
-
+var shuffled_word = words[Math.floor(Math.random() * words.length)];
+return shuffled_word
 };
-
